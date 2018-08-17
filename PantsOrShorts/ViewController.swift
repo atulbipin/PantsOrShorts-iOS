c685f27d71677af150309d99efa6c1a697b058fb
Atul Bipin <atulbipin@gmail.com>
2018-08-16 19:19:10 -0700

    Initial Commit

---
+++ PantsOrShorts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PantsOrShorts
+//
+//  Created by Atul Bipin on 2018-08-16.
+//  Copyright © 2018 Atul Bipin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
