8130b1de3971d036a7d8ad81cbbd53e9b4488b8d
Atul Bipin <atulbipin@gmail.com>
2018-08-16 20:45:07 -0700

    Initial commit

--- PantsOrShorts/ViewController.swift
+++ PantsOrShorts/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    let locationManager = CLLocationManager()
+    
+    @IBAction func onButtonPress() {
+        locationManager.requestLocation()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let latitude = locations.last?.coordinate.latitude, let longitude = locations.last?.coordinate.longitude {
+            print("\(latitude),\(longitude)")
+            
+            lookUpCurrentLocation { geoLocation in
+                print(geoLocation?.locality ?? "Unknown geo location")
+            }
+        } else {
+            print("No coordinates")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    func lookUpCurrentLocation(completionHandler: @escaping (CLPlacemark?) -> Void) {
+        // Use the last reported location.
+        if let lastLocation = self.locationManager.location {
+            let geocoder = CLGeocoder()
+            
+            // Look up the location and pass it to the completion handler
+            geocoder.reverseGeocodeLocation(lastLocation, completionHandler: { (placemarks, error) in
+                if error == nil {
+                    let firstLocation = placemarks?[0]
+                    completionHandler(firstLocation)
+                }
+                else {
+                    // An error occurred during geocoding.
+                    completionHandler(nil)
+                }
+            })
+        }
+        else {
+            // No location was available.
+            completionHandler(nil)
+        }
+    }
 }
 
---
+++ PantsOrShorts/WeatherAPI.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherAPI.swift
+//  PantsOrShorts
+//
+//  Created by Atul Bipin on 2018-08-16.
+//  Copyright © 2018 Atul Bipin. All rights reserved.
+//
+
+import Foundation
+
+public class Weather {
+    private let API_KEY = "572a6acca7b36cc3b77ec5ab99658f3c"
+}
