4a4263568d4e72fa0076ee05c8d5824f415a3866
Atul Bipin <atulbipin@gmail.com>
2018-08-18 15:25:10 -0700

    Updated location services

--- PantsOrShorts/Services/Location.swift
+++ PantsOrShorts/Services/Location.swift
@@ -17,12 +17,30 @@ public class Location: NSObject, CLLocationManagerDelegate {
     public override init() {
         super.init()
         
-        self.locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
     }
     
     public func getCurrentLocation(completion: @escaping (CurrentLocation?) -> Void) {
-        locationManager.requestLocation()
         self.currentLocationCallback = completion
+        self.startReceivingLocationChanges()
+    }
+    
+    private func startReceivingLocationChanges() {
+        let authorizationStatus = CLLocationManager.authorizationStatus()
+        if authorizationStatus != .authorizedWhenInUse && authorizationStatus != .authorizedAlways {
+            // User has not authorized access to location information.
+            return
+        }
+        // Do not start services that aren't available.
+        if !CLLocationManager.locationServicesEnabled() {
+            // Location services is not available.
+            return
+        }
+        // Configure and start the service.
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.distanceFilter = 100.0  // In meters.
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
     }
     
     // MARK: - CLLocationManagerDelegate
